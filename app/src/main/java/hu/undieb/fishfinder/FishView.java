69f953c987d6ef6f10ec005538bbdb44f13b9286
Letsktor <vamat@mailbox.unideb.hu>
2024-02-22 19:07:56 +0100

    made some smaller upgrades

--- app/src/main/java/hu/undieb/fishfinder/FishView.java
+++ app/src/main/java/hu/undieb/fishfinder/FishView.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
